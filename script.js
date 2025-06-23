24e303f6ed966a31615581d9343c1fe066efa800
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2025-06-23 22:26:49 +0200

    Added more JS, UI and finishing touches

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  let cleanedPoem = response.data.answer
+    .replace(/^```html\n?/, "") // Remove starting ```html (with optional newline)
+    .replace(/```$/, ""); // Remove ending ```
 
   new Typewriter("#poem", {
-    strings: "Nic dwa razy się nie zdarza i nie zdarzy",
+    strings: cleanedPoem,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "7e04f2c0fabfb2db3d42449315oe8tf5";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a poem in Polish language about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Polish poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
